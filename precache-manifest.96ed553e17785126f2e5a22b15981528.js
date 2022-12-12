37c1a443bb82610ea99e92b60afb9d927624035f
Arturo <arturovelazquez94@gmail.com>
2022-12-12 12:02:40 -0600

    Updates

---
+++ precache-manifest.96ed553e17785126f2e5a22b15981528.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a64d770d944f9c90dac53d910ff8f55",
+    "url": "/arturo-velazquez-vargas.github.io/index.html"
+  },
+  {
+    "revision": "9e2a0a61e0362d045bbe",
+    "url": "/arturo-velazquez-vargas.github.io/static/css/2.2341c6f5.chunk.css"
+  },
+  {
+    "revision": "093126cea5ee5a716db2",
+    "url": "/arturo-velazquez-vargas.github.io/static/css/main.a1a2ac69.chunk.css"
+  },
+  {
+    "revision": "9e2a0a61e0362d045bbe",
+    "url": "/arturo-velazquez-vargas.github.io/static/js/2.02525709.chunk.js"
+  },
+  {
+    "revision": "006f60af7ead3db49606992e81f44a62",
+    "url": "/arturo-velazquez-vargas.github.io/static/js/2.02525709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "093126cea5ee5a716db2",
+    "url": "/arturo-velazquez-vargas.github.io/static/js/main.7dc03b1c.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/arturo-velazquez-vargas.github.io/static/js/main.7dc03b1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbbe90329d0f2ca9761d",
+    "url": "/arturo-velazquez-vargas.github.io/static/js/runtime-main.34d28068.js"
+  },
+  {
+    "revision": "85064a97348166467bd7bc2ad80ccdce",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/404.85064a97.svg"
+  },
+  {
+    "revision": "9ba4b7cbe7f072ea0fe50ec0d4b6c97b",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/404.9ba4b7cb.jpg"
+  },
+  {
+    "revision": "192d8f70e8dbebb4b03f98db1a6434c7",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/500.192d8f70.jpg"
+  },
+  {
+    "revision": "a0320da1a9ae65d73f6af7780883975c",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/500.a0320da1.svg"
+  },
+  {
+    "revision": "6aae84abf0090324eb1daa7eef6bfdd8",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/bootstrap-5-logo.6aae84ab.svg"
+  },
+  {
+    "revision": "95abb40d53edf7f917949e8757cdf5c6",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/bs5-illustrations.95abb40d.svg"
+  },
+  {
+    "revision": "ed3cd4b507f8209412bc426332f23589",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/canada.ed3cd4b5.svg"
+  },
+  {
+    "revision": "935e70467bab0575ad62110dfae45476",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/forgot-password.935e7046.jpg"
+  },
+  {
+    "revision": "968aaa24eeffdd779e4fc9c27c5cb730",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/france.968aaa24.svg"
+  },
+  {
+    "revision": "d810f62137d64a0a58035c80489b7d54",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/germany.d810f621.svg"
+  },
+  {
+    "revision": "612e617f5d72975e63e53d793d757785",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/italy.612e617f.svg"
+  },
+  {
+    "revision": "1f905d23af144bc76e488f00d516de2e",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/japan.1f905d23.svg"
+  },
+  {
+    "revision": "32442a8e647bf33b852807cd91c492e4",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/laravel-logo.32442a8e.svg"
+  },
+  {
+    "revision": "69f2db42fdfeb91a54fe1573b6b102c9",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/lock.69f2db42.jpg"
+  },
+  {
+    "revision": "b2d9831a6290a67a05fa6d65bb67d226",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/mockup-calendar-presentation.b2d9831a.png"
+  },
+  {
+    "revision": "fc6d4c754fd2b26a2e4775bc91b61bbc",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/mockup-map-presentation.fc6d4c75.png"
+  },
+  {
+    "revision": "77a4c6a89e893405dd125243971c3d61",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/mockup-presentation.77a4c6a8.png"
+  },
+  {
+    "revision": "00a526136ebe6ca9adbd18a0dcb9064d",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/overview.00a52613.jpg"
+  },
+  {
+    "revision": "4d26e151b224eaa88df2742351073c56",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/profile-picture-1.4d26e151.jpg"
+  },
+  {
+    "revision": "487133c5319652023f459ea4e3a4720a",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/profile-picture-2.487133c5.jpg"
+  },
+  {
+    "revision": "9a250e5ca0df93fd467f77c4d7e3d3d4",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/profile-picture-3.9a250e5c.jpg"
+  },
+  {
+    "revision": "ccd796b31c6ca0c778c6065b3b6a20b7",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/profile-picture-4.ccd796b3.jpg"
+  },
+  {
+    "revision": "baf7bf753ca889c7db833fb0db47abac",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/profile-picture-5.baf7bf75.jpg"
+  },
+  {
+    "revision": "055a91979264664a1ee12b9453610d82",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/profile.055a9197.png"
+  },
+  {
+    "revision": "c90966040cdfe3d1eab0fa3839933b78",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/public-sans-all-400-normal.c9096604.woff"
+  },
+  {
+    "revision": "a0548e6f8b9bdaf34723546609ea95f6",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/public-sans-latin-400-normal.a0548e6f.woff2"
+  },
+  {
+    "revision": "44ad56999611068efaf464ea11dbf556",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/public-sans-latin-ext-400-normal.44ad5699.woff2"
+  },
+  {
+    "revision": "204e6804820923f62e3ee31527c05af3",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/public-sans-vietnamese-400-normal.204e6804.woff2"
+  },
+  {
+    "revision": "9394d800b9cadc76bc0dd4ecb6f9edcf",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/react-hero-logo.9394d800.svg"
+  },
+  {
+    "revision": "5e51d949230c7a92d64c73604e5a3243",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/react-logo-primary.5e51d949.svg"
+  },
+  {
+    "revision": "f7d45c01a0efd126cc1030d5cf78fd84",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/react-logo-transparent.f7d45c01.svg"
+  },
+  {
+    "revision": "b779e4fd2515e28f0233a071f3d46104",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/react-logo.b779e4fd.svg"
+  },
+  {
+    "revision": "99d034b6cea27baae946ce783434954f",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/react-mockup.99d034b6.png"
+  },
+  {
+    "revision": "0a4cc9cd55c1c298542e559d34622a48",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/reset-password.0a4cc9cd.jpg"
+  },
+  {
+    "revision": "43fe41840c256623201cc14f63415614",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/settings.43fe4184.jpg"
+  },
+  {
+    "revision": "ea051702c568e9d0199b6ddc1a52fff4",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/sign-in.ea051702.jpg"
+  },
+  {
+    "revision": "6774df144d28e4f7c55c1f7725823465",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/sign-up.6774df14.jpg"
+  },
+  {
+    "revision": "58ee1d5a0a93d00df3f6c12170ebd3ff",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/signin.58ee1d5a.svg"
+  },
+  {
+    "revision": "bbd344a3e9c39621e69a64c02e4b11de",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/themesberg-logo.bbd344a3.svg"
+  },
+  {
+    "revision": "105e5059b0997718976601d17fd2a04b",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/themesberg.105e5059.svg"
+  },
+  {
+    "revision": "b98aee40114ed3eaae525ce4ec63b0c3",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/transactions.b98aee40.jpg"
+  },
+  {
+    "revision": "e5564902e2642c5e6e2e98e68a7d41f5",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/united-kingdom.e5564902.svg"
+  },
+  {
+    "revision": "36ab476e5e55f496749ee61897a9cfb5",
+    "url": "/arturo-velazquez-vargas.github.io/static/media/united-states-of-america.36ab476e.svg"
+  }
+]);
